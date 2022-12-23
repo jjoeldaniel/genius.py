f4f88342229f2bddfcd5ad52a12e6f78b93188d4
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 10:56:28 -0800

    Basic implementation
    Only contains search function and constructor

---
+++ genius.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from track import Track
+from rauth import OAuth2Service
+
+class Genius:
+
+    endpoint = 'https://api.genius.com'
+
+    def __init__(self, client_id, client_secret, client_access_token):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.client_access_token = client_access_token
+
+    def search(self, query):
+        endpoint = f'{self.endpoint}/search'
+        data = {'q': query}
+        headers = {'Authorization': f'Bearer {self.client_access_token}'}
+
+        response = requests.get(endpoint, params=data, headers=headers)
+
+        tracks = []
+
+        for hits in response.json()['response']['hits']:
+            track = Track(track_info=hits)
+            tracks.append(track)
+
+        return tracks
