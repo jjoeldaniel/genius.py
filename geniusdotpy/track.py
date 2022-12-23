2aaeaeb7096c5dd30f585b3800aca65b81a31331
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 14:21:42 -0800

    Checks if track contains keys first

--- geniusdotpy/track.py
+++ geniusdotpy/track.py
@@ -7,18 +7,30 @@ class Track:
         self.name = track_info['title_with_featured']
         self.artist = track_info['primary_artist']['name']
         self.title = track_info['title']
+        self.path = track_info['path']
         self.title_with_featured = track_info['title_with_featured']
         self.full_title = track_info['full_title']
         self.url = track_info['url']
         self.id = track_info['id']
-        self.language = track_info['language']
         self.featured_artists = track_info['featured_artists']
-        self.header_image_thumbnail_url = track_info['header_image_thumbnail_url']
-        self.header_image_url = track_info['header_image_url']
-        self.path = track_info['path']
-        self.song_art_image_thumbnail_url = track_info['song_art_image_thumbnail_url']
-        self.art_image_url = track_info['song_art_image_url']
-        self.page_views = track_info['stats']['pageviews']
+
+        if 'label' in track_info:
+            self.language = track_info['language']
+        
+        if 'header_image_thumbnail_url' in track_info:
+            self.header_image_thumbnail_url = track_info['header_image_thumbnail_url']
+
+        if 'header_image_url' in track_info:
+            self.header_image_url = track_info['header_image_url']
+
+        if 'song_art_image_thumbnail_url' in track_info:
+            self.song_art_image_thumbnail_url = track_info['song_art_image_thumbnail_url']
+        
+        if 'song_art_image_url' in track_info:
+            self.art_image_url = track_info['song_art_image_url']
+
+        if 'pageviews' in track_info['stats']:
+            self.page_views = track_info['stats']['pageviews']
 
     def __str__(self):
         return self.full_title
