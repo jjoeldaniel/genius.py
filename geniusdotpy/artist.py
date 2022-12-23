58bd5d3fd7979ad4e950e1779ccde7419df7ada9
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 15:36:33 -0800

    Added docstring for list_tracks()

--- geniusdotpy/artist.py
+++ geniusdotpy/artist.py
@@ -6,6 +6,8 @@ class Artist:
         self.tracks_info = tracks_info
 
     def list_tracks(self):
+        """Returns a list of Track objects for an Artist"""
+
         tracks = []
 
         for track in self.tracks_info:
