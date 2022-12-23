6e527db642c965d4851118288f24eb7419267311
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 12:32:31 -0800

    Added example.py

---
+++ geniusdotpy/example.py
@@ -0,0 +1,13 @@
+import os
+from genius_builder import *
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+client_access_token = os.getenv('CLIENT_ACCESS_TOKEN')
+
+genius = GeniusBuilder(client_id=client_id, client_secret=client_secret, client_access_token=client_access_token)
+
+for track in genius.search(query='Kendrick Lamar'):
+    print(track)
+
+print(genius.search_by_id(378195).url)
