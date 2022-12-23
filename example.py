ac1e2e6be6f123b80af1c29fe5a4bbb3a3099185
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 15:26:59 -0800

    More documentation

--- example.py
+++ example.py
@@ -6,15 +6,21 @@ from dotenv import load_dotenv
 # See https://pypi.org/project/python-dotenv/
 load_dotenv()
 
+# Get environment variables
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 client_access_token = os.getenv('CLIENT_ACCESS_TOKEN')
 
+# Create a GeniusBuilder object
 genius = GeniusBuilder(client_id=client_id, client_secret=client_secret, client_access_token=client_access_token)
 
+# Returns a list of songs containing query
 print(genius.search(query='Kendrick Lamar')[0])
+
+# Returns song matching song ID
 print(genius.search_by_id(378195))
 
+# Returns an Artist object containing all songs by artist
 artist = genius.search_artist(artist_id=16775)
 for song in artist.list_tracks():
     print(song)
