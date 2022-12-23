b743f7feee4d902a3edd0718217ec134848c2986
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 13:09:50 -0800

    Added more documentation

--- geniusdotpy/example.py
+++ geniusdotpy/example.py
@@ -1,5 +1,10 @@
 import os
 from genius_builder import *
+from dotenv import load_dotenv
+
+# Example uses dotenv to load environment variables from .env file
+# See https://github.com/cdimascio/dotenv-java
+load_dotenv()
 
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
@@ -10,4 +15,4 @@ genius = GeniusBuilder(client_id=client_id, client_secret=client_secret, client_
 for track in genius.search(query='Kendrick Lamar'):
     print(track)
 
-print(genius.search_by_id(378195).url)
+print(genius.search_by_id(378195).page_views)
