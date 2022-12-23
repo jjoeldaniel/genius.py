169d079208b34bebb63b4726165dc4818fa51b83
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-23 12:10:58 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python wrapper for Genius API'
+LONG_DESCRIPTION = 'Python wrapper for Genius API'
+
+# Setting up
+setup(
+    name="genius.py",
+    version=VERSION,
+    author="jjoeldaniel",
+    author_email="<mail@joeldanielrico@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python', 'genius', 'api-wrapper'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
