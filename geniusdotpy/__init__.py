038b8bd2feee3f9c38d4a91a11c0a0defecf638a
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 15:26:48 -0800

    Updated __init__

--- geniusdotpy/__init__.py
+++ geniusdotpy/__init__.py
@@ -2,3 +2,4 @@ from geniusdotpy.genius_builder import GeniusBuilder
 from geniusdotpy.artist import Artist
 from geniusdotpy.track import Track
 from geniusdotpy.lyrics import Lyrics
+from geniusdotpy.album import Album
