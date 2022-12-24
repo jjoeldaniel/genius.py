f60146562432b9912546fbc5e6aa89b08794a584
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 15:24:53 -0800

    Added album.py

---
+++ geniusdotpy/album.py
@@ -0,0 +1,12 @@
+class Album:
+
+    def __init__(self, album_info):
+        self.album_info = album_info
+        self.id = album_info['id']
+        self.name = album_info['name']
+        self.url = album_info['url']
+        self.path = album_info['api_path']
+        self.cover_art_url = album_info['cover_art_url']
+
+    def __str__(self):
+        return self.name
