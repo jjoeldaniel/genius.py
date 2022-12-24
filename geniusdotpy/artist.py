8cff9f1fc127f6ca024f52fc02d88fb3d8b6c538
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 14:41:48 -0800

    Added additional Artist fields

--- geniusdotpy/artist.py
+++ geniusdotpy/artist.py
@@ -7,3 +7,7 @@ class Artist:
         self.artist_info = artist_info
         self.tracks_info = tracks_info
         self.tracks = [Track(track_info=track_info) for track_info in self.tracks_info]
+        self.id = self.artist_info['id']
+        self.name = self.artist_info['name']
+        self.url = self.artist_info['url']
+        self.path = self.artist_info['api_path']
