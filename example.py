ca51412b204ce21c2f26a0cdaec70c650807eaa0
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 13:46:47 -0800

    Example now includes use of Lyrics class

--- example.py
+++ example.py
@@ -11,10 +11,13 @@ genius = GeniusBuilder(client_id=client_id, client_secret=client_secret, client_
 # Returns a list of songs containing query
 print(genius.search(query='Kendrick Lamar')[0])
 
-# Returns song matching song ID
-print(genius.search_by_id(378195))
-
 # Returns an Artist object containing all songs by artist
 artist = genius.search_artist(artist_id=16775)
-for song in artist.tracks:
-    print(song)
+print(artist.tracks[0])
+
+# Returns song matching song ID
+track = genius.search_by_id(378195)
+print(track)
+
+# Prints the lyrics of the song
+print(track.lyrics)
