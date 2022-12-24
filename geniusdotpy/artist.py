073b42b3ed059de5fadc5250802e3273b32d3503
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 06:19:04 -0800

    Artist track list now initialized in constructor

--- geniusdotpy/artist.py
+++ geniusdotpy/artist.py
@@ -5,14 +5,8 @@ class Artist:
 
     def __init__(self, tracks_info):
         self.tracks_info = tracks_info
-
-    def list_tracks(self):
-        """Returns a list of Track objects for an Artist"""
-
-        tracks = []
+        self.tracks = []
 
         for track in self.tracks_info:
             track = Track(track_info=track)
-            tracks.append(track)
-
-        return tracks
+            self.tracks.append(track)
