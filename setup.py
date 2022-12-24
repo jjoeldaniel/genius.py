2732ae56d5a5a37d6840fc23d75ecd5fa8cde523
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-24 08:56:14 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 this_directory = Path(__file__).parent
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 DESCRIPTION = 'Python wrapper for Genius API'
 long_description = (this_directory / "README.md").read_text()
 
