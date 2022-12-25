e78f8d4b09c4b3f8d398116fa274ea3a09940c8f
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-25 11:45:55 -0800

    Added additional attributes

--- geniusdotpy/album.py
+++ geniusdotpy/album.py
@@ -5,10 +5,23 @@ class Album:
     def __init__(self, album_info):
         self.album_info = album_info
         self.id = album_info['id']
-        self.name = album_info['name']
         self.url = album_info['url']
         self.path = album_info['api_path']
-        self.cover_art_url = album_info['cover_art_url']
+
+        if 'cover_art_url' in album_info:
+            self.cover_art_url = album_info['cover_art_url']
+
+        # Fallback name to full_title if name is not present and vice versa
+        if 'name' in album_info:
+            self.name = album_info['name']
+        else:
+            self.name = album_info['full_title']
+
+        if 'full_title' in album_info:
+            self.full_title = album_info['full_title']
+        else:
+            self.full_title = album_info['name']
+
 
     def __str__(self):
         return self.name
