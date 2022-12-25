463b89e3dbaf18ee67771ced23aa117be02a0dae
Joel <100006388+joelrico@users.noreply.github.com>
2022-12-25 12:31:10 -0800

    Updated format_json() docstring

--- geniusdotpy/utils.py
+++ geniusdotpy/utils.py
@@ -1,5 +1,12 @@
 import json
 
 def format_json(json_object):
-    """Formats JSON object to be more readable"""
+    """Formats JSON object to be more readable
+    
+    Keyword arguments:
+        json_object -- The JSON object to format
+
+    Returns:
+        JSON object
+    """
     return json.dumps(json_object, indent=2)
