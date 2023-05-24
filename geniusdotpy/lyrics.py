4f7f1ae227c1326c2de4e9f8386fd28047ff51bc
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-23 20:47:47 -0700

    updated class name

--- geniusdotpy/lyrics.py
+++ geniusdotpy/lyrics.py
@@ -3,9 +3,12 @@ import requests
 
 
 class Lyrics:
-
     def __init__(self, track_url):
         html = requests.get(track_url).content
-        soup = BeautifulSoup(html, 'html.parser')
-        self.content = soup.find('div', class_='Lyrics__Container-sc-1ynbvzw-6 YYrds').get_text(separator='\n')
-        self.content_by_line = self.content.split('\n')
+        soup = BeautifulSoup(html, "html.parser")
+
+        self.content = soup.find(
+            "div", class_="Lyrics__Container-sc-1ynbvzw-5"
+        ).get_text(separator="\n")
+
+        self.content_by_line = self.content.split("\n")
