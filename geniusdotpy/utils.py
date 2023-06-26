a7d6656108d1a04b7f079a7fadfd28a6d38542f7
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-25 22:09:53 -0700

    add types and additional docs

--- geniusdotpy/utils.py
+++ geniusdotpy/utils.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def format_json(json_object):
+def format_json(json_object: dict):
     """Formats JSON object to be more readable
 
     Keyword arguments:
@@ -15,10 +15,17 @@ def format_json(json_object):
     return json.dumps(json_object, indent=2)
 
 
-def retrieve_lyrics(track_url):
-        html = requests.get(track_url).content
-        soup = BeautifulSoup(html, "html.parser")
+def retrieve_lyrics(track_url: str):
+    """
+    Retrieves the lyrics of a song from Genius.
+    
+    Keyword arguments:
+        track_url -- The URL of the song
+    """
+    
+    html = requests.get(track_url).content
+    soup = BeautifulSoup(html, "html.parser")
 
-        return soup.find(
-            "div", class_="Lyrics__Container-sc-1ynbvzw-5"
-        ).get_text(separator="\n")
+    return soup.find(
+        "div", class_="Lyrics__Container-sc-1ynbvzw-5"
+    ).get_text(separator="\n")
