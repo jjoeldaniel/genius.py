9c8f92b813c80652baf016ac751f9c429e81d63f
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-12-29 10:37:46 -0800

    Update Genius API calls and print track and artist information

--- example.py
+++ example.py
@@ -4,28 +4,31 @@ from geniusdotpy.track import Track
 
 def main():
     # Get your client access token from https://genius.com/api-clients
-    client_access_token = "_J6d3sXCgXEwBm-MmtDyLUCSAgMEfrKmospTdqrr4ZsorkLuHBG3wUu5WllnKUzQ"
+    client_access_token = "token"
 
     # Create a GeniusBuilder object
     genius = Genius(client_access_token=client_access_token)
 
     # Search for a track by ID
-    print(genius.search_track_by_id(378195))
+    track1 = genius.search_track_by_id(378195)
+    print(repr(track1))
 
     # Search artist by ID
-    print(genius.search_artist(16775))
+    artist1 = genius.search_artist(track1.artist.id)
+    print(repr(artist1))
 
     # Search for a track by query
-    print(genius.search("Beat It")[0])
+    track3 = genius.search("Beat It")[0]
+    print(repr(track3))
 
     # Search tracks by artist
     tracks: list[Track] = genius.search_track_by_artist(artist_id=16775)
-    print(tracks[0])
+    track4 = tracks[0]
+    print(repr(track4))
 
     # Retrieve lyrics
-    tracks[0].get_lyrics()
-
-    print(tracks[0].lyrics)
+    track4.get_lyrics()
+    print(track4.lyrics)
 
 
 if __name__ == "__main__":
